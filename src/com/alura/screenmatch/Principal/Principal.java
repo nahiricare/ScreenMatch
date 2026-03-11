928951701a7cabb71ba83395583fd6d48de29bd5
nahiricare <foxydevsolution@gmail.com>
2026-03-11 18:43:17 -0300

    Agrega funcionalidades a ScreenMatch con uso de listas

---
+++ src/com/alura/screenmatch/Principal/Principal.java
@@ -0,0 +1,66 @@
+package com.alura.screenmatch.Principal;
+
+import com.alura.screenmatch.calculos.CalculadoraDeTiempo;
+import com.alura.screenmatch.calculos.FiltroRecomendacion;
+import com.alura.screenmatch.modelos.Episodio;
+import com.alura.screenmatch.modelos.Pelicula;
+import com.alura.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula("El señor de los anillos", 2021);
+        miPelicula.setDuracionEnMinutos(180);
+        System.out.println("Duración de la película: " + miPelicula.getDuracionEnMinutos());
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(8);
+        miPelicula.evalua(5);
+        miPelicula.evalua(10);
+        System.out.println("Total de evaluaciones: " + miPelicula.getTotalDeEvaluaciones());
+        System.out.println(miPelicula.calculaMediaEvaluaciones());
+
+
+        Serie lost = new Serie("Lost", 2000);
+        lost.muestraFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duracion de la série: " + lost.getDuracionEnMinutos());
+
+        Pelicula otraPelicula = new Pelicula("Avatar", 2023);
+        otraPelicula.setDuracionEnMinutos(200);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluido(miPelicula);
+        calculadora.incluido(otraPelicula);
+        calculadora.incluido(lost);
+        System.out.println(calculadora.getTiempoTotal());
+
+        FiltroRecomendacion filtro = new FiltroRecomendacion();
+        filtro.filtra(miPelicula);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizaciones(300);
+        filtro.filtra(episodio);
+
+
+        var peliculaNahir = new Pelicula("Matilda", 2001);
+        peliculaNahir.setDuracionEnMinutos(100);
+
+        ArrayList listaDePeliculas = new ArrayList<>();
+        listaDePeliculas.add(peliculaNahir);
+        listaDePeliculas.add(otraPelicula);
+        listaDePeliculas.add(miPelicula);
+        System.out.println("Tamaño de la lista: " + listaDePeliculas.size());
+        System.out.println("Primera película: " + listaDePeliculas.get(0));
+        System.out.println(listaDePeliculas);
+
+
+
+
+    }
+}
